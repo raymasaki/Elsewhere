08e0cb3544c8ad8abc297d86bb54344dc85600a5
Ray Masaki <ray@raymasaki.com>
2016-02-27 10:45:38 -0500

    better responsive and angle resets on mouseleave

--- experiment_1/js/app.js
+++ experiment_1/js/app.js
@@ -4,12 +4,22 @@ $(document).ready(function() {
    var W = $(window).width();
    var H = $(window).height();
 
-   // object width & height
-   var oW = W / 6;
-   var oH = oW * 0.1367;
+   $('.space').width(W);
 
    // number of cells per row and columns
    var numX = 6;
+
+   // if smaller than 1000 && 700
+   if (W < 1000 && W > 700) {
+      numX = 4;
+   } else if (W <= 700) {
+      numX = 3;
+   }
+
+   // object width & height
+   var oW = W / numX;
+   var oH = oW * 0.1367;
+
    var numY = Math.floor(H / oH);
 
    // total number of cells
@@ -31,43 +41,31 @@ $(document).ready(function() {
       $cell.append('<img src="images/elsewhere.svg">');
 
       $('.space').append($cell);
-      var pos = {x : $cell.position().left, y : $cell.position().top};
+      var pos = {x : $cell.position().left + oW/2, y : $cell.position().top + oH/2};
 
       cellPos.push(pos);
    }
 
-
-   // $(window).mousemove(function(e) {
-   //    var X = cW - e.pageX;
-   //    var Y = cH - e.pageY;
-   //    var angle = Math.atan(Y/X) * 180/Math.PI;
-   //
-   //    console.log(angle);
-   //    $('.cell').css('transform', 'rotate(' + angle + 'deg)');
-   //
-   // });
-
-   // $(window).resize(function() {
-   //    cellPos = [];
-   //
-   //    $('.cell').each(function() {
-   //       var pos = {x : $cell.position().left, y : $cell.position().top};
-   //
-   //       cellPos.push(pos);
-   //    });
-   // });
-
    $(window).mousemove(function(e) {
 
+      // turns off transition when mouse is in window
+      $('.cell').css('transition', 'none');
+
       for (var i = 0; i < numTotal; i++) {
-         var X = cellPos[i].x - (e.pageX + 110);
-         var Y = cellPos[i].y - (e.pageY - 150);
+         var X = cellPos[i].x - (e.pageX);
+         var Y = cellPos[i].y - (e.pageY - 200);
          var angle = Math.atan(Y/X) * 180/Math.PI;
 
          $( 'div[num=' + i +']' ).css('transform', 'rotate(' + angle + 'deg)');
-
       }
 
+   });
+
+   // if mouse is outside of window make it reset angle
+   $(document).mouseleave(function() {
+      $('.cell').css('transition', 'all 250ms');
+
+      $('.cell').css('transform', 'rotate(0deg)');
    });
 
 });
